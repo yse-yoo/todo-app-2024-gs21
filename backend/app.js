20596ce06693a96637da2440ca84df87248676bc
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-05 14:54:32 +0900

    update

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(expres.json())
+app.use(express.json())
 app.use(cors())
 
 // Webサーバを待機（listen）
