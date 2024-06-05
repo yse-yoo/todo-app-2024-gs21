03db178a76d01a9ac2b8192e272b5a82cd005435
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-05 14:10:07 +0900

    update

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 
+// .envファイル読み込み 
+dotenv.config()
+
+const HOST = process.env.HOST
+const PORT = process.env.PORT
+
+console.log(HOST, PORT)
