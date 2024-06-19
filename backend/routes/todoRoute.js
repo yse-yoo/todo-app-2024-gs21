2580a339c232671dcc46e40ddbd318f3d07a61e7
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-19 14:08:14 +0900

    update

--- backend/routes/todoRoute.js
+++ backend/routes/todoRoute.js
@@ -1,4 +1,25 @@
 const express = require('express')
 const router = express.Router();
 
+router.get('/todo/get', (req, res) => {
+    var todos = [
+        { 
+            id: 1,
+            title: "買い物",
+            completed: false,
+        },
+        { 
+            id: 2,
+            title: "アルバイト",
+            completed: false,
+        },
+        { 
+            id: 3,
+            title: "銀行に行く",
+            completed: false,
+        },
+    ]
+    res.json(todos) 
+})
+
 module.exports = router
